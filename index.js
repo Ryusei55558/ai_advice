b136cbfa07f03142d927c434baa6c0b936635e1f
Ryusei55558 <145423624+Ryusei55558@users.noreply.github.com>
2024-01-25 17:09:00 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,14 @@
 $(function () {
-    // カレンダー
-    $(function () {
-        $('input[name="date"]').datepicker({
-            dateFormat: 'yy/mm/dd',
-        });
-    });
-
-    // 参加人数分の氏名欄を生成
-    $('#form-number').click(function () {
-        $('#form-name').empty();
-        var num = $('input[name="number"]:checked').val();
-        for (i = 0; i < num; i++) {
-            $('#form-name').append(
-                `<input class="form-control w-100 mt-1" name="names[]" maxlength="10">`
-            );
-        }
-    });
-
     // 送信
     $('form').submit(function () {
-        var date = $('input[name="date"]').val();
-        var number = $('input[name="number"]:checked').val();
-        var names = '';
-        $('input[name="names[]"]').each(function (i, elm) {
-            names += $(elm).val() + '、';
-        });
-        names = names.slice(0, -1);
-
+        var names = = $('input[name="names"]').val();
         var university = $('input[name="university"]').val();
         var grade = $('input[name="grade"]').val();
         var industry = $('input[name="industry"]').val();
         var company = $('input[name="company"]').val();
         var content = $('textarea[name="content"]').val();
 
-        var msg = `希望日：${date}\n人数：${number}\n名前：${names}\n大学：${university}\n学年：${grade}\n志望業界：${industry}\n志望企業：${company}\n相談内容：${content}`;
+        var msg = `名前：${names}\n大学：${university}\n学年：${grade}\n志望業界：${industry}\n志望企業：${company}\n相談内容：${content}`;
         sendText(msg);
 
         return false;

beefd71a22606f94103b98b3b07dde8ba48947bb
Ryusei55558 <145423624+Ryusei55558@users.noreply.github.com>
2024-01-25 17:35:24 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 $(function () {
+    // カレンダー
+    $(function () {
+        $('input[name="date"]').datepicker({
+            dateFormat: 'yy/mm/dd',
+        });
+    });
+
+    // 参加人数分の氏名欄を生成
+    $('#form-number').click(function () {
+        $('#form-name').empty();
+        var num = $('input[name="number"]:checked').val();
+        for (i = 0; i < num; i++) {
+            $('#form-name').append(
+                `<input class="form-control w-100 mt-1" name="name" maxlength="10">`
+            );
+        }
+    });
+    
     // 送信
     $('form').submit(function () {
         var names = = $('input[name="name"]').val();

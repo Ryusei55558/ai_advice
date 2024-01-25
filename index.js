41e74e6da3795b019e7510e0ecce805efe905448
Ryusei55558 <145423624+Ryusei55558@users.noreply.github.com>
2024-01-25 18:11:43 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
 $(function () {
-    // カレンダー
-    $(function () {
-        $('input[name="date"]').datepicker({
-            dateFormat: 'yy/mm/dd',
-        });
-    });
-
-    // 参加人数分の氏名欄を生成
-    $('#form-number').click(function () {
-        $('#form-name').empty();
-        var num = $('input[name="number"]:checked').val();
-        for (i = 0; i < num; i++) {
-            $('#form-name').append(
-                `<input class="form-control w-100 mt-1" name="name" maxlength="10">`
-            );
-        }
-    });
-    
     // 送信
     $('form').submit(function () {
-        var names = = $('input[name="name"]').val();
+        var names = $('input[name="name"]').val();
         var university = $('input[name="university"]').val();
         var grade = $('input[name="grade"]').val();
         var industry = $('input[name="industry"]').val();
@@ -34,6 +16,22 @@ $(function () {
 });
 
 /*
+
+$(function () {
+    // 送信
+    $('form').submit(function () {
+        var company = $('input[name="company"]').val();
+        var text_len = $('input[name="text_len"]').val();
+        var content = $('textarea[name="content"]').val();
+
+        var msg = `【AI ES添削】\n提出先企業：${company}\n文字数：${text_len}\nES内容：${content}`;
+        sendText(msg);
+
+        return false;
+    });
+});
+
+
 $(function () {
     // カレンダー
     $(function () {
